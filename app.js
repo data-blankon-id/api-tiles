7dc595cf0c480ddb63d3fa3bda40d0ab142c2243
Dhi Aurrahman <diorahman@rockybars.com>
2015-05-31 01:08:47 +0700

    Moar https blankon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (window.location.host == host && window.location.protocol != 'https:')
   window.location.protocol = 'https:'
 
 var map = L.map('map').setView([-1.269160, 116.825264], 3);
-var tiles = L.tileLayer('http://data.blankon.id/api/tiles?s={s}&z={z}&x={x}&y={y}', {
+var tiles = L.tileLayer('https://data.blankon.id/api/tiles?s={s}&z={z}&x={x}&y={y}', {
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
